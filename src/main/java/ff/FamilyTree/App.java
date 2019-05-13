45dbe61d081f9805472766ab5418199ef60b8903
ungentrified <blackichan200@gmail.com>
2019-05-13 06:51:08 +0100

    first commit uncompleted family class

---
+++ src/main/java/ff/FamilyTree/App.java
@@ -0,0 +1,13 @@
+package ff.FamilyTree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ff/FamilyTree/FamilyTreeSt.java
@@ -0,0 +1,82 @@
+package ff.FamilyTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FamilyTreeSt {
+	
+	public static void main(String abc[]) {
+		
+		
+	}
+	
+	public class Family{
+	List<FamilyMember> FamilyTreeSt;
+		
+		public Family() {
+			familyMemeber = new Array
+		}
+		
+		public boolean male(String name) {
+			return false;
+			
+		}
+		
+public boolean female(String name) {
+	return false;
+			
+		}
+		
+public boolean isMale(String name) {
+	return false;
+	
+}
+
+public boolean isFemale(String name) {
+	return false;
+	
+}
+public boolean setParent(String childName, String parentName) {
+	return false;
+	
+}
+
+public String[] getParents(String name) {
+	return null;
+	
+}
+
+public String[] getChildren(String name) {
+	return null;
+	
+}
+
+
+		
+	}
+	
+	
+	public class FamilyMember{
+		String name;
+		String gender;
+		
+		public FamilyMember() {
+			String[] parents = new String[2];;
+			List<String> children = new ArrayList<String>();
+			
+		}
+		
+		public String name(){
+			
+			return name;
+		}
+		
+		public String gender(){
+			
+			return gender;
+		}
+		
+	}
+
+}
